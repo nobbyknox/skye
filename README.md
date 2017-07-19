ccd71f7d7cfc9d302ec267eb3e4afc016b7417da
Nobby Knox <nobbyknox@gmail.com>
2017-07-19 22:08:38 +0200

    Initial commit
    I have a rough, but working bully leader election algorithm
    going. Nothing fancy (yet).

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+*Skye* is a secret little project at the moment. It will change in the future.
---
+++ config/develop.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    logger: {
+        name: 'skye',
+        level: 'trace',
+        src: true
+    },
+
+    mqtt: {
+        broker: {
+            host: '127.0.0.1',
+            username: 'user',
+            password: 'secret'
+        }
+    }
+};
