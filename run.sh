ee2fc4991197ea394d7fb58a23ad0b9eef950160
Nobby Knox <nobbyknox@gmail.com>
2017-07-24 18:43:09 +0200

    New config property to enable task delegator

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ export logger_src="true"
 export mqtt_broker_host="172.0.0.20"
 export mqtt_broker_username="null"
 export mqtt_broker_password="null"
+export enable_delegator="false"
 
 clear; npm run start:no-reload
