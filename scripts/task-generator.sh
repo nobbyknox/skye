29feffeae9b62d61f3e1ac43b51f5ea45ffebbdc
Nobby Knox <nobbyknox@gmail.com>
2017-07-25 20:10:11 +0200

    Add bash script that generates work requests

--- scripts/task-generator.sh
+++ scripts/task-generator.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-taskNum=`shuf -i 0-3 -n 1`
+
+taskNum=0
+
+while [ true ]; do
+    taskNum=`shuf -i 0-1 -n 1`
+
+    if [ $taskNum = 0 ]; then
+        echo "Requesting image processing"
+        mosquitto_pub -t skye/process-image -m '{}'
+    elif [ $taskNum = 1 ]; then
+        echo "Requesting message delivery"
+        mosquitto_pub -t skye/send-notification -m '{}'
+    else
+        echo "Task number $taskNum is not supported"
+    fi
+
+    sleep 2;
+done
