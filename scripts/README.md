e1c0d9d1429f702e067d156c4793f14aec8bf30c
Nobby Knox <nobbyknox@gmail.com>
2017-08-03 14:34:19 +0200

    Create run template script in "scripts" directory

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# README
+
+## Run Skye
+
+Copy `run-template.sh` to the project root folder and rename to `run-private.sh` or `_run.sh`. Then, configure and run.
--- scripts/run-template.sh
+++ scripts/run-template.sh
@@ -3,7 +3,7 @@
 export logger_name="skye"
 export logger_level="debug"
 export logger_src="true"
-export mqtt_broker_host="172.0.0.20"
+export mqtt_broker_host="127.0.0.1"
 export mqtt_broker_username="null"
 export mqtt_broker_password="null"
 export enable_delegator="false"
